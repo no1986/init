5456b67cef84fc8b2c05dddda986bb73aa3b9e66
no1986 <n.ohkawa1221@gmail.com>
2022-09-20 16:51:21 +0900

    change pecoとdirenvをgoからaptに変更

--- install.sh
+++ install.sh
@@ -59,7 +59,7 @@ function package_install(){
              libgnutls28-dev libgpm-dev libgtk-3-dev libjansson-dev libjpeg-dev liblockfile-dev libm17n-dev \
              libmagick++-6.q16-dev libncurses5-dev libotf-dev libpng-dev librsvg2-dev libselinux1-dev libtiff-dev \
              libxaw7-dev libxml2-dev openssh-client texinfo xaw3dg-dev zlib1g-dev ruby cmigemo p7zip-full \
-             ripgrep \
+             ripgrep direnv peco \
              > /dev/null""" 4
     }
     e_cmd "sudo apt-get update > /dev/null" 2
@@ -113,8 +113,6 @@ function env_install(){
         export PATH=${GOPATH}/bin:${PATH}
 
         e_cmd "go install github.com/x-motemen/ghq@latest > /dev/null" 4
-        e_cmd "go install github.com/peco/peco/cmd/peco@latest > /dev/null" 4
-        e_cmd "go install github.com/direnv/direnv@latest > /dev/null" 4
     }
     function nodenv_install(){
         e_cmd "anyenv install nodenv -s > /dev/null" 4
